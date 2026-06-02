471791863ed7f97f9381c47a4596dd4fa3b2e8a3
T.Nikitha <tnikitha042@gmail.com>
2026-06-02 13:25:55 +0530

    Add files via upload

---
+++ Iris_Flower_Classification/iris.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+df = pd.read_csv("Iris.csv")
+
+# Features
+X = df[['sepal_length',
+        'sepal_width',
+        'petal_length',
+        'petal_width']]
+
+# Target
+y = df['species']
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+# Predict
+predictions = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, predictions)
+
+print("Accuracy:", accuracy)
+
+
